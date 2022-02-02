8740af96a117034988d3b64ce83aa724e3d8d640
Karen <to.karenngo@gmail.com>
2022-02-01 19:10:37 -0500

    Add one to many association between Store and Employee, and create 6 employees to designated stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end 
+
+
+class Employee
+  belongs_to :store
+end 
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Karen", last_name: "Ngo", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Leung", hourly_rate: 60)
+@store2.employees.create(first_name: "Bob", last_name: "Baker", hourly_rate: 60)
+@store2.employees.create(first_name: "Joe", last_name: "Brown", hourly_rate: 60)
+@store2.employees.create(first_name: "Carol", last_name: "Johnson", hourly_rate: 60)
